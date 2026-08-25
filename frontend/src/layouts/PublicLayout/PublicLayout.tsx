f4dc1730472cdee381f7de6f384ce9ab0f6c9143
Sreelakshmi K <sreelakshmikavenge3i3@gmail.com>
2026-08-25 15:16:49 +0530

    route

--- frontend/src/layouts/PublicLayout/PublicLayout.tsx
+++ frontend/src/layouts/PublicLayout/PublicLayout.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Header from '../../components/organisms/Header/Header'
+import Footer from '../../components/organisms/Footer/Footer'
+import { Outlet } from 'react-router'
 
 const PublicLayout = () => {
   return (
-    <div>
-      
+    <div className="flex min-h-screen w-full flex-col bg-bg">
+      <Header />
+      <main className="flex flex-1 w-full flex-col">
+        <Outlet />
+      </main>
+      <Footer />      
     </div>
   )
 }
