245d9993b8cc549b6dea9b5474e0e5df6855aa09
Sreelakshmi K <sreelakshmikavenge3i3@gmail.com>
2026-08-28 22:32:20 +0530

    create Header component

--- frontend/src/components/organisms/Header/Header.tsx
+++ frontend/src/components/organisms/Header/Header.tsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import type { HeaderProps } from '../../../types/header.types'
 
-const Header = () => {
+const Header = ({ left, center, right }: HeaderProps) => {
   return (
-    <header className="">
-
+    <header className="w-full py-2 px-10 h-15 bg-bg border border-b border-primary/50">
+      {
+      left && !center && !right && 
+        <div className="flex items-center justify-start h-full">
+          <div className="flex-shrink-0 gap-5">
+            {left}
+          </div>
+        </div>
+      }
+      {
+      !left && center && !right && 
+        <div className="flex items-center justify-center h-full">
+          <div className="flex-shrink-0">
+            {center}
+          </div>
+        </div>
+      }
+      {
+      !left && !center && right && 
+        <div className="flex items-center justify-center h-full">
+          <div className="flex-shrink-0">
+            {right}
+          </div>
+        </div>
+      }
+      {left && right && 
+       <div className="flex items-center justify-between h-full">
+        <div className="flex-shrink-0">
+          {left}
+        </div>
+        <div className="flex-shrink-0">
+          {center}
+        </div>
+        <div className="flex-shrink-0">
+          {right}
+        </div>
+      </div>}
     </header>
   )
 }
---
+++ frontend/src/types/header.types.ts
@@ -0,0 +1,5 @@
+export type HeaderProps = {
+    left?: React.ReactNode
+    center?: React.ReactNode
+    right?: React.ReactNode
+}
